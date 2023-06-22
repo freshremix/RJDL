31a34ee12c30c571bdb6c41cf344210628387f85
freshremix <90133873+freshremix@users.noreply.github.com>
2023-06-22 14:00:20 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,84 +1,71 @@
-"""GUI application that download music from 'www.radiojavan.com' """
-
-__version__ = '1.0'
-__author__ = 'soroush safari'
-
-
 import sys
 import re
 import webbrowser
-
-from radio import RadioJavan
-
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QFileDialog
-from PyQt5.QtGui import QMovie
-from PyQt5.QtCore import QByteArray
 from functools import partial
-
+from radio import RadioJavan
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtCore import Qt
 
 def download(url):
-	"""
-	open web browser from url scraped from action function
-	"""
-	webbrowser.open(url)
+    """
+    Open web browser with the URL scraped from the action function.
+    """
+    webbrowser.open(url)
 
 def action(url):
-	"""
-	call scraper
-	:param url: url song in www.radiojavan.com
-	"""
-	if url.text():
-		url = url.text() # grap text inside the QlineEdit
-		match = re.match(r"(https://www.radiojavan.com/mp3s)",url) # match for correct pattern
-		if match: 
-			radio_javan = RadioJavan(url=url) # create instance for scraper
-			if radio_javan.scrap():
-				final_url = radio_javan.scrap() # grab url from scraper
-				download_btn.show() # show download button
-				download_btn.clicked.connect(partial(download,final_url)) # call download function to open url
-			else:
-				result_lbl.setText("you must enter mp3 link")
-		else:
-			result_lbl.setText("please enter correct url")
-	else:
-		result_lbl.setText("please enter url !!!")
-
+    """
+    Call the scraper.
+    :param url: URL of the song in www.radiojavan.com
+    """
+    if url.text():
+        url_text = url.text().strip()  # Get text from QLineEdit and remove leading/trailing spaces
+        match = re.match(r"(https://www.radiojavan.com/mp3s)", url_text)  # Match for the correct pattern
+        if match:
+            radio_javan = RadioJavan(url=url_text)  # Create an instance of the scraper
+            final_url = radio_javan.scrap()  # Scrape the URL
+            if final_url:
+                download_btn.show()  # Show the download button
+                download_btn.clicked.connect(partial(download, final_url))  # Connect the download function to open the URL
+            else:
+                result_lbl.setText("Failed to retrieve the download URL.")
+        else:
+            result_lbl.setText("Please enter a correct URL.")
+    else:
+        result_lbl.setText("Please enter a URL.")
 
 # Create an instance of QApplication
 app = QApplication(sys.argv)
 
 # Create an instance of your application's GUI
 window = QWidget()
-window.setWindowTitle('Radio javan downloader')
-window.setGeometry(500, 300, 700, 300) # set size window
+window.setWindowTitle('Radio Javan Downloader')
+window.setGeometry(500, 300, 700, 300)  # Set the window size
+window.setFixedSize(700, 300)  # Fix the window size
 
-# label that show what user must do
-main_lbl = QLabel('Please enter url :', parent=window)
+# Label that shows what the user should do
+main_lbl = QLabel('Please enter the URL:', parent=window)
 main_lbl.move(140, 50)
 
-# input box
+# Input box
 url_input = QLineEdit(parent=window)
-url_input.setFixedWidth(500)
-url_input.setFixedHeight(40)
-url_input.move(100, 80)
+url_input.setGeometry(100, 80, 500, 40)
+url_input.setPlaceholderText('Enter the URL of the song')
+url_input.setFocus()
 
-# button to start scrap url
-btn = QPushButton('find :)',parent=window)
-btn.clicked.connect(partial(action,url_input)) # when button press call action()
-btn.move(210, 140)
+# Button to start scraping the URL
+btn = QPushButton('Find', parent=window)
+btn.setGeometry(210, 140, 280, 40)
+btn.clicked.connect(partial(action, url_input))  # Call the action() function when the button is pressed
 
-# download button
-download_btn = QPushButton('Download',parent=window)
-download_btn.move(300, 140)
+# Download button
+download_btn = QPushButton('Download', parent=window)
+download_btn.setGeometry(300, 140, 100, 40)
 download_btn.hide()
 
+# Label to show the result
+result_lbl = QLabel(parent=window)
+result_lbl.setGeometry(50, 190, 600, 40)
+result_lbl.setAlignment(Qt.AlignCenter)
 
-# show result to this label
-result_lbl = QLabel('',parent=window)
-result_lbl.setFixedWidth(600)
-result_lbl.move(50, 190)
-
-
-window.show() # show window
-sys.exit(app.exec_()) # keep alive app in mainloop
+window.show()  # Show the window
+sys.exit(app.exec_())  # Keep the app running in the main loop
